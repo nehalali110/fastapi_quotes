48dbdfc64f6102fd414aee82e3356e88e5472d88
nehalali110 <nehalalikhan110@gmail.com>
2025-10-22 02:06:09 +0500

    Completed GET requests --Heading on to POST

---
+++ database/storage.py
@@ -0,0 +1,4 @@
+quotes = [
+    {"id": 1, "text": "Be the change you wish to see in the world", "author": "Gandhi"},
+    {"id": 2, "text": "The only way to do great work is to love what you do", "author": "Steve Jobs"}
+]
--- main.py
+++ main.py
@@ -1,7 +1,39 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from database.storage import quotes  
 
 app = FastAPI()
 
 @app.get('/')
-def welcome_home():
-    return "Hey there what are you doing?"
+@app.get('/home')
+def home():
+    return "Welcome to the Quotes Server"
+
+
+@app.get('/quotes')
+def get_quotes(author: str = None, search: str = None, limit: int = None):
+    if author == None and search == None and limit == None:
+        return quotes
+
+    filtered_quotes = []
+    while True:
+        base_list = get_base_list(filtered_quotes, quotes)
+        if author:
+            filtered_quotes = [quote for quote in base_list if quote['author'].lower() == author.lower()]
+            author = None
+            continue
+        elif search:
+            filtered_quotes = [quote for quote in base_list if search.lower() in quote['text'].lower()]
+            search = None
+            continue
+        elif limit:
+            filtered_quotes = base_list[:limit]
+            limit = None
+            continue
+        else:
+            return filtered_quotes
+        
+
+def get_base_list(filtered_quotes, quotes):
+    if filtered_quotes:
+        return filtered_quotes
+    return quotes
