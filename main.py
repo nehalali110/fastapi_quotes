bab8a44de237c8a4ac5c20ff492e75070a823f3f
nehalali110 <nehalalikhan110@gmail.com>
2025-10-23 01:57:20 +0500

    Extra GET parameters issues are resolved now.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from database.storage import quotes  
+from pydantic import BaseModel, Extra
 
 app = FastAPI()
 
@@ -9,25 +10,31 @@ def home():
     return "Welcome to the Quotes Server"
 
 
+class QuoteQuery(BaseModel, extra=Extra.forbid):
+    author: str | None = None
+    search: str | None = None
+    limit: int | None = None
+
 @app.get('/quotes')
-def get_quotes(author: str = None, search: str = None, limit: int = None):
-    if author == None and search == None and limit == None:
+def get_quotes(params: QuoteQuery = Depends()):
+    print(params)
+    if params.author == None and params.search == None and params.limit == None:
         return quotes
 
     filtered_quotes = []
     while True:
         base_list = get_base_list(filtered_quotes, quotes)
-        if author:
-            filtered_quotes = [quote for quote in base_list if quote['author'].lower() == author.lower()]
-            author = None
+        if params.author:
+            filtered_quotes = [quote for quote in base_list if quote['author'].lower() == params.author.lower()]
+            params.author = None
             continue
-        elif search:
-            filtered_quotes = [quote for quote in base_list if search.lower() in quote['text'].lower()]
-            search = None
+        elif params.search:
+            filtered_quotes = [quote for quote in base_list if params.search.lower() in quote['text'].lower()]
+            params.search = None
             continue
-        elif limit:
-            filtered_quotes = base_list[:limit]
-            limit = None
+        elif params.limit:
+            filtered_quotes = base_list[:params.limit]
+            params.limit = None
             continue
         else:
             return filtered_quotes
