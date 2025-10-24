57b0c30f4cc085afed22e6d737ae76417eefed35
nehalali110 <nehalalikhan110@gmail.com>
2025-10-24 15:33:10 +0500

    POST request completed

--- main.py
+++ main.py
@@ -63,6 +63,6 @@ def get_base_list(filtered_quotes, quotes):
         return filtered_quotes
     return quotes
 
-def add_timestamp(self):
+def add_timestamp():
     current_datetime = datetime.now()
     return current_datetime.strftime("%d-%m-%Y %H:%M:%S")
